95a6b31994d5074dc5494db4004013f62e2672c0
Atheena Renjith <athenarenjit1@cet.ac.in>
2020-12-08 18:17:37 +0530

    benefit layout added

---
+++ ConvAI_FE/src/components/benefits.js
@@ -0,0 +1,62 @@
+import React from "react";
+
+const benefits = () => {
+  return (
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <h3 class="text-center">Advantage</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
+            repudiandae a ipsum minima ut eaque assumenda nesciunt quia,
+            eligendi 
+          </p>
+        </div>
+        <div class="col-md-4">
+          <h3 class="text-center">Advantage</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
+            repudiandae a ipsum minima ut eaque assumenda nesciunt quia,
+            eligendi 
+          </p>
+        </div>
+        <div class="col-md-4">
+          <h3 class="text-center">Advantage</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
+            repudiandae a ipsum minima ut eaque assumenda nesciunt quia,
+            eligendi 
+          </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-4">
+          <h3 class="text-center">Advantage</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
+            repudiandae a ipsum minima ut eaque assumenda nesciunt quia,
+            eligendi 
+          </p>
+        </div>
+        <div class="col-md-4">
+          <h3 class="text-center">Advantage</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
+            repudiandae a ipsum minima ut eaque assumenda nesciunt quia,
+            eligendi 
+          </p>
+        </div>
+        <div class="col-md-4">
+          <h3 class="text-center">Advantage</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
+            repudiandae a ipsum minima ut eaque assumenda nesciunt quia,
+            eligendi 
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default benefits;
---
+++ ConvAI_FE/src/components/landingPage.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const landingPage = () => {
+  return (
+    <div>
+      <h1 class="text-center">Conversational AI on Tabular Data</h1>
+      <p class="text-center">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit, delectus
+        magni tempore iste hic amet harum recusandae obcaecati nihil, iusto quod
+        illo quas repudiandae itaque voluptatum voluptas dignissimos eveniet
+        cumque. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit,
+        delectus magni tempore iste hic amet harum recusandae obcaecati nihil,
+        iusto quod illo quas repudiandae itaque voluptatum voluptas dignissimos
+        eveniet cumque.
+      </p>
+    </div>
+  );
+};
+
+export default landingPage;
