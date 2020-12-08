93799da2281948fa2f55ff1fa12eeb24adb752c6
Atheena Renjith <athenarenjit1@cet.ac.in>
2020-12-08 09:03:01 +0530

    Basic layout added

---
+++ ConvAI_FE/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import InputCard from './components/inputcard'
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Conversational AI on Tabular Data</h1>
+     <InputCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ ConvAI_FE/src/components/inputcard.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const inputcard = () => {
+  return (
+    <div class="col-md-12">
+      <input type="button" value="Upload csv" /> <br/>
+      <input type="button" value="Upload schema" /> <br/>
+      <label>Enter your query</label><br/>
+      <input type="text" /><br/>
+      <button>Get SQL Query</button><br/>
+      <button>Get Query Output</button><br/>
+    </div>
+  );
+};
+
+export default inputcard;
