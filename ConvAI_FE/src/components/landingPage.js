4a84b55d823ffe6eef7f5d8de1fcf7356642e24f
LukoseJ10 <63980194+LukoseJ10@users.noreply.github.com>
2020-12-08 21:56:09 +0530

    landing.js updated with the text content.

--- ConvAI_FE/src/components/landingPage.js
+++ ConvAI_FE/src/components/landingPage.js
@@ -5,13 +5,18 @@ const landingPage = () => {
     <div>
       <h1 class="text-center">Conversational AI on Tabular Data</h1>
       <p class="text-center">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit, delectus
-        magni tempore iste hic amet harum recusandae obcaecati nihil, iusto quod
-        illo quas repudiandae itaque voluptatum voluptas dignissimos eveniet
-        cumque. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit,
-        delectus magni tempore iste hic amet harum recusandae obcaecati nihil,
-        iusto quod illo quas repudiandae itaque voluptatum voluptas dignissimos
-        eveniet cumque.
+          In this fast technologically advancing world, it has become
+          important for the humans to interact with computers to provide
+          assistance in many fields like medicine, education, space
+          research, etc. Retrieval of the required information from the
+          database is a tedious process. In order to extract information
+          from the database, the user must have a prior knowledge on
+          Database Management System (DBMS), which is a software
+          developed to store and manipulate the data in a database.
+          Hence a non-technical user faces difficulty in extracting the
+          data. To find a solution for such a problem and facilitate
+          human interaction with computers, Natural Language Processing(NLP) techniques are used.The two main challenges that have to be solved simultaneously are:
+          Question complexity (depth) & Domain size (breadth). Our project is a humble attempt to solve the Domain size challenge, by extracting data with natural queries from .csv files
       </p>
     </div>
   );
