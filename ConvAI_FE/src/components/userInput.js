c4ac5f8f6e5ed546604f1aa06624ac5df067917b
Dhruv Peter <dhruveldhopeter@gmail.com>
2020-12-08 23:40:43 +0530

    View available csv added

--- ConvAI_FE/src/components/userInput.js
+++ ConvAI_FE/src/components/userInput.js
@@ -3,12 +3,18 @@ import React from "react";
 const inputcard = () => {
   return (
     <div class="text-center">
-      <input type="button" value="Upload csv" /> <br/>
-      <input type="button" value="Upload schema" /> <br/>
-      <label>Enter your query</label><br/>
-      <input type="text" /><br/>
-      <button>Get SQL Query</button><br/>
-      <button>Get Query Output</button><br/>
+      <input type="button" value="View available csv" /> <br />
+      <input type="button" value="Upload csv" /> <br />
+      <input type="button" value="Upload schema" /> <br />
+      <input type="button" value="Clear" /> <br />
+      <label>Enter your query</label>
+      <br />
+      <input type="text" />
+      <br />
+      <button>Get SQL Query</button>
+      <br />
+      <button>Get Query Output</button>
+      <br />
     </div>
   );
 };
